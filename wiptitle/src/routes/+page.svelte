f49e10ad13564a0a61c641a7993d824475a4dffd
Yves Velasquez <vyves@csu.fullerton.edu>
2023-12-16 17:43:25 -0800

    Skeleton code

--- wiptitle/src/routes/+page.svelte
+++ wiptitle/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>WIP CONVERTER</h1>
+<body>
+</body>
+ 
+<style>
+h1
+    {
+    text-align: center;
+    color: rgb(255, 255, 255);
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-style: normal;
+    font-size: 2em;
+    }
+body
+{
+    background-color: #9B9FB5;
+    margin: 0 auto;
+    padding: 1em;
+}
+</style>
