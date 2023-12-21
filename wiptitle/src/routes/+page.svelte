0e42efaf9f010cfc6c13a55704d55eb5adcc3d39
Yves Velasquez <vyves@csu.fullerton.edu>
2023-12-21 14:05:16 -0800

    Google Sign in Button
    Added google sign in button, removed unnecessary files
    
    TODO: move HTML -> TS for button

--- wiptitle/src/routes/+page.svelte
+++ wiptitle/src/routes/+page.svelte
@@ -1,32 +1,37 @@
+
 <h1>Swap & Play</h1>
 
 <body>
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <script src="https://apis.google.com/js/api.js"></script>
+    <script src="https://accounts.google.com/gsi/client" async></script>
+    <div id="g_id_onload"
+    data-client_id="708450403388-esrq8qrfid8ssgv8sifedlli9j1vae1g.apps.googleusercontent.com"
+    data-context="signin"
+    data-ux_mode="popup"
+    data-login_uri="http://localhost:5173/home"
+    data-nonce=""
+    data-itp_support="true">
+</div>
 
-
-    <div class="sign-in-wrapper">
-        <div class="sign-in-content">
-                <div>
-                    <button class="btn-auth"    type="submit">
-                    <img class="btn-auth-img" src='/google_signin_buttons/web/1x/ btn_google_signin_dark_pressed_web.png' alt='google sign in'/>
-                    </button>
-                </div>
-        </div>
-    </div>
+<div class="g_id_signin"
+    data-type="standard"
+    data-shape="pill"
+    data-theme="outline"
+    data-text="signin_with"
+    data-size="large"
+    data-logo_alignment="left">
+</div>
 </body>
+
  
 <style>
-h1
-    {
+h1 {
     text-align: center;
     color: rgb(255, 255, 255);
     font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     font-style: normal;
     font-size: 2em;
     }
-body
-{
+body {
     background-color: #9B9FB5;
     margin: 0 auto;
     padding: 1em;
--- wiptitle/src/routes/home/+page.svelte
+++ wiptitle/src/routes/home/+page.svelte
@@ -1,16 +1,2 @@
-<script lang="ts">
-    
-</script>
-
-<html lang="en">
-  <meta charset="utf-8">
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>swap and play</title>
-  </head>
-<body>
-</body>
-</html>
-
-<style></style>
+<h1>Swap & Play</h1>
 
