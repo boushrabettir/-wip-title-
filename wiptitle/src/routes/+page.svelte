0f804f8933f5d6e4dc1d946bc3cb1f92bda9eb24
HallowsYves <83028938+HallowsYves@users.noreply.github.com>
2024-05-21 12:48:48 -0700

    Hard reset

---
+++ wiptitle/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<h1>WIP CONVERTER</h1>
+<body>
+</body>
+
+<style>
+h1
+    {
+    text-align: center;
+    color: rgb(255, 255, 255);
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-style: normal;
+    font-size: 2em;
+    }
+body
+{
+    background-color: #9B9FB5;
+    margin: 0 auto;
+    padding: 1em;
+}
+</style>
